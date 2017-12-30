70193b9eedca0c7c2c29a1de488cecf1083cd0ce
rakeshsagar64 <rakeshsagar64@gmail.com>
2017-12-30 11:41:54 +0530

    login register page structure design part 1

--- controller/mainController.js
+++ controller/mainController.js
@@ -7,6 +7,14 @@ app.get('/',function (request,response) {
   response.render('home');
 });
 
+app.get('/login',(request,response) => {
+  response.render('candidateLogin');
+});
+
+app.get('/register',(request,response) => {
+  response.render('candidateRegister');
+});
+
 app.get('/recruiter',function(request,response){
 
   response.render('recruiterPortal');
--- controller/recruiterController.js
+++ controller/recruiterController.js
@@ -1,6 +1,17 @@
 let recruiterController=function(app){
 
 console.log("recruiter Controller");
+
+app.get('recruiterLogin',(request,response) => {
+  response.render('recruiterLogin');
+});
+
+
+app.get('recruiterRegister',(request,response) => {
+  response.render('recruiterRegister');
+});
+
+
 app.get("postJob",function (request,response) {
     res.render("postJob");
 });
